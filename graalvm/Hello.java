bd08ab9f33f503376818ed3bf8bea59247528bdc
Curtis Rueden <ctrueden@wisc.edu>
2023-06-07 14:43:07 +0200

    Add non-working GraalVM attempt

---
+++ graalvm/Hello.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Hello {
+  public static void main(String... args) {
+    // Uncomment the following line to watch GraalVM barf on Swing:
+    //JOptionPane.showMessageDialog(null, "Hello, World!");
+    System.out.println("Hello world");
+  }
+}
